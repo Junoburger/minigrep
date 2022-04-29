9c3cdc1277db7f53f0c545ee6c9fe9fd36aaa8a5
Junoburger <juno.burger@pm.me>
2022-04-29 08:53:35 +0200

    Displaying metadata on frontend

--- _MD_tests/metadata_test2/target/rls/debug/deps/hello_wasm-4434cdff453ab130.d
+++ _MD_tests/metadata_test2/target/rls/debug/deps/hello_wasm-4434cdff453ab130.d
@@ -1,5 +1,5 @@
-/Users/home/Development/Projects/RUST/projects/metadata_test2/target/rls/debug/deps/hello_wasm-4434cdff453ab130.rmeta: src/lib.rs
+/Users/home/Development/Projects/RUST/projects/_MD_tests/metadata_test2/target/rls/debug/deps/hello_wasm-4434cdff453ab130.rmeta: src/lib.rs
 
-/Users/home/Development/Projects/RUST/projects/metadata_test2/target/rls/debug/deps/hello_wasm-4434cdff453ab130.d: src/lib.rs
+/Users/home/Development/Projects/RUST/projects/_MD_tests/metadata_test2/target/rls/debug/deps/hello_wasm-4434cdff453ab130.d: src/lib.rs
 
 src/lib.rs:
